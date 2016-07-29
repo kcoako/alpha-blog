4af2bbf82f2b03889472e41e7fb07bae6cd8e278
Kim Angelo Co <co_kim_angelo@hotmail.com>
2016-07-29 11:17:40 +0000

    Style changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def destroy
 
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   def create
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to articles_path(@article)
     else
       render 'new'
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def update
 
     if @article.update(article_params)
-      flash[:notice] = "article was succesfully updated"
+      flash[:success] = "article was succesfully updated"
       redirect_to articles_path(@article)
     else
       render 'edit'
