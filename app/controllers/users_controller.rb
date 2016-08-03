9c78ff10bdb451c0100f8ba843563e51702a2c02
Kim Angelo Co <co_kim_angelo@hotmail.com>
2016-08-03 10:13:09 +0000

    partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,27 @@ class UsersController < ApplicationController
     debugger
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "You have successfully created an account"
+      flash[:success]= "You have successfully created an account"
       redirect_to articles_path
       else
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your acount was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
 
   private
   def user_params
